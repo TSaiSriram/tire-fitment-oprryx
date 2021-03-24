6b6fb521fb5464f73aa72254708c35ef900d55f9
SaiSriramTekumalla <stekumalla@miraclesoft.com>
2021-03-24 14:20:55 +0530

    Update fitment.module.ts

--- src/fitment/fitment.module.ts
+++ src/fitment/fitment.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
-
+import {  HttpClientModule } from '@angular/common/http';
 import { reducers } from './store'
-
+import { EffectsModule } from '@ngrx/effects';
+import { TireEffects } from './store/effects/vechile.effect';
 import { FitmentContainerComponent } from './fitment-container/fitment-container.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
+    HttpClientModule,
     StoreModule.forFeature('fitment', reducers),
+    EffectsModule.forRoot([TireEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
   ],
   declarations: [FitmentContainerComponent],
+
   exports: [FitmentContainerComponent]
 })
 export class FitmentModule { }
